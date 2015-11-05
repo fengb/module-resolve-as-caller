73a1077bdb46cdc7de2750439a1fccd9857406b8
fengb <contact@fengb.info>
2015-11-04 19:05:04 -0600

    Unrolled silly function.apply

--- test/support/index.js
+++ test/support/index.js
@@ -1,13 +1,13 @@
 var moduleResolveParent = require('../..')
 
-var supportResolve = module.exports = function () {
-  return moduleResolveParent.apply(null, arguments)
+var supportResolve = module.exports = function (path) {
+  return moduleResolveParent(path)
 }
 
-supportResolve.nested = function () {
-  return supportResolve.apply(null, arguments)
+supportResolve.nested = function (path) {
+  return supportResolve(path)
 }
 
-supportResolve.deeply = function () {
-  return supportResolve.nested.apply(null, arguments)
+supportResolve.deeply = function (path) {
+  return supportResolve.nested(path)
 }
